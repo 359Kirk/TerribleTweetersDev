1a52a6a3d1a9a723bfdb00f867e080ffd6180e48
359Kirk <brianabishopaz@gmail.com>
2022-04-15 13:33:14 -0700

    Audio Sliders added w/ Build

---
+++ Terrible Tweeters/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject settingsUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+    }
+
+    public void GameSettings()
+    {
+        settingsUI.SetActive(true);
+    }
+
+    public void Close()
+    {
+        settingsUI.SetActive(false);
+    }
+}
